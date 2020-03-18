77ba6662a422069a3f8727883862f1f1dbe56447
Osawerengeka <mr.georgegevork@gmail.com>
2020-03-18 23:46:44 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubModule
+{
+        public class Security
+        {
+            public int hashfunc(string pass)
+            {
+                int hash = 0;
+                for (int i = 1; i < pass.Length; i++)
+                {
+                    hash = hash + pass[i] * pass[0];
+
+                }
+                return hash;
+            }
+        }
+}
